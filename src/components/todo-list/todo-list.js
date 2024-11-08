784d45f96970140318698c85647badcc7a7dbd8d
Atabek666 <Kotos.fox@gmail.com>
2024-11-08 21:54:19 +0500

    step 24 Refactoring main components with setState() function for events

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import App from './components/app';
+import TodoListItem from '../todo-list-item/todo-list-item';
+import './todo-list.css';
 
-ReactDOM.render(<App />,
-    document.getElementById('root'));
+const TodoList = ({ todos, onDeleted }) => {
+
+    const elements = todos.map((item) => {
+        const { id, ...itemProps } = item;
+
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem {...itemProps }
+                onDeleted = {() => onDeleted(id)}/>
+            </li>
+        );
+    });
+
+    return (
+        <ul className="list-group todo-list">
+            { elements }
+        </ul>
+    );
+};
+
+export default TodoList;
